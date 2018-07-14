ae308c2c4acd7ba3954ca0ebddb86adcad95a924
dohzoh <dohzoh@outlook.com>
2018-07-14 13:33:23 +0900

    first commit

--- html-import/src/example.vue
+++ html-import/src/example.vue
@@ -5,8 +5,11 @@
 </template>
 
 <script>
-export default {
-	name : 'example',
+var doc = document.currentScript.ownerDocument;
+var template = doc.querySelector('template');
+
+Vue.component('my-component', {
+	template,
 	data() {
 		return {
 			name : 'world'
@@ -17,11 +20,12 @@ export default {
 			this.name = 'foobar';
 		}
 	}
-};
+});
 </script>
 
 <style lang="less" scoped>
 div {
 	font-weight: bold;
+	color: red;
 }
 </style>
--- template/entry.js
+++
@@ -1,6 +0,0 @@
-// ----------------------------------------------------------------------
-// BROWSER COMPONENT ENTRY FILE
-// ----------------------------------------------------------------------
-
-import Component from './src/__FILE__';
-Vue.component(Component.name, Component);
