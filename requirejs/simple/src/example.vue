4ad586b9204130065b6135d07e53a65e893a712f
dohzoh <dohzoh@outlook.com>
2018-07-14 17:45:52 +0900

    add requirejs

---
+++ requirejs/simple/src/example.vue
@@ -0,0 +1,33 @@
+<template>
+	<div @click="changeName()">
+		Hello, {{name}}.
+	</div>
+
+  <style>
+    div {
+      font-weight: bold;
+      color: red;
+    }
+  </style>  
+</template>
+
+<script>
+define(['Vue'], (Vue) =>{
+
+  return Vue.extend({
+    template: template,
+    data() {
+      return {
+        name: "world"
+      };
+    },
+    methods: {
+      changeName() {
+        this.name = "foobar";
+      }
+    }
+  });
+
+});
+</script>
+
