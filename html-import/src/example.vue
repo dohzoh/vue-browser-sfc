f3f228f06cf5fb9b26ba473f48bc741a54973480
dohzoh <dohzoh@outlook.com>
2018-07-14 13:46:35 +0900

    fixed indent

--- html-import/src/example.vue
+++ html-import/src/example.vue
@@ -6,26 +6,26 @@
 
 <script>
 var doc = document.currentScript.ownerDocument;
-var template = doc.querySelector('template');
+var template = doc.querySelector("template");
 
-Vue.component('my-component', {
-	template,
-	data() {
-		return {
-			name : 'world'
-		};
-	},
-	methods : {
-		changeName() {
-			this.name = 'foobar';
-		}
-	}
+Vue.component("my-component", {
+  template,
+  data() {
+    return {
+      name: "world"
+    };
+  },
+  methods: {
+    changeName() {
+      this.name = "foobar";
+    }
+  }
 });
 </script>
 
 <style lang="less" scoped>
 div {
-	font-weight: bold;
-	color: red;
+  font-weight: bold;
+  color: red;
 }
 </style>
